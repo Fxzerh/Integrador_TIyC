44028aa4a4424d7c1fa2ed98359e78f46fc4d839
Alfonso German <germanalfonso788@gmail.com>
2026-06-14 19:14:19 -0300

    Implementacion para ver los archivo en el panel de compactar

--- Controllers/compactarController.py
+++ Controllers/compactarController.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import QTableWidget, QMessageBox, QTableWidgetItem, QHeaderView
+from PyQt6.QtCore import QUrl
 from Clases.cargarArchivo import Cargar
 import os
 
@@ -24,7 +25,7 @@ class CompactarController:
 
         # ---------------------------- ACCIONES Y EVENTOS ---------------------------------------------------------------------------------------------------------
         self.mainWindow.subirArchivoC_btn.clicked.connect(lambda: self.cargar.seleccionar_y_guardar(self))
-
+        self.mainWindow.tableFileC.itemClicked.connect(self.mostrar)
     
 
     def cambiarPanel (self, indice):
@@ -59,3 +60,15 @@ class CompactarController:
                     self.mainWindow.tableFileC.insertRow(rowPosition)
                     self.mainWindow.tableFileC.setItem(rowPosition, 0, QTableWidgetItem(f))             # Nombre
                     self.mainWindow.tableFileC.setItem(rowPosition, 1, QTableWidgetItem(tamaño_str))    # Tamaño
+
+    def mostrarArchivo(self, nombre_archivo):
+        ruta_completa = os.path.join(self.carpetaArchivos, nombre_archivo)
+        if os.path.exists(ruta_completa):
+            url_local = QUrl.fromLocalFile(ruta_completa)   # Transformamos la ruta de Windows a una URL que entienda el componente web        
+            self.mainWindow.viewC.setUrl(url_local)         # Setteamos la vista web que lo dibuje en pantalla
+
+    def mostrar(self, item):
+        fila = item.row()      # Obtenemos el número de la fila que el usuario tocó
+        nombre = self.mainWindow.tableFileC.item(fila, 0)    # Extraemos el objeto celda de la columna 0 (Nombre) en esa fila
+        nombreArchivo = nombre.text()    # Sacamos el texto plano (el nombre real del archivo)
+        self.mostrarArchivo(nombreArchivo)     # Le pasamos el nombre a la función que lo carga en el visor web
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtCore import Qt, QCoreApplication
+from PyQt6.QtWebEngineCore import QWebEngineProfile, QWebEngineSettings
 from Controllers.mainWindowController import MainWindow
 
 QCoreApplication.setAttribute(Qt.ApplicationAttribute.AA_ShareOpenGLContexts)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    # Activamos el plugin de PDFs de forma global para todo el programa
+    QWebEngineProfile.defaultProfile().settings().setAttribute(QWebEngineSettings.WebAttribute.PluginsEnabled, True)
+    
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
